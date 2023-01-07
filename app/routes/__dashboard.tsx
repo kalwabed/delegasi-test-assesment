9e90896ce064e2e97093eb1b846a541f14a216d0
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-01-07 19:23:11 +0700

    style: adding more padding bottom

--- app/routes/__dashboard.tsx
+++ app/routes/__dashboard.tsx
@@ -66,7 +66,7 @@ export default function WithSubnavigation() {
           <Collapse in={isOpen} animateOpacity>
             <MobileNav />
           </Collapse>
-          <Flex flexDir="column" w="full" h="full" px={4} bgColor="gray.50">
+          <Flex flexDir="column" w="full" h="full" px={4} bgColor="gray.50" pb={8}>
             <DashboardContainer />
             <Outlet />
           </Flex>
