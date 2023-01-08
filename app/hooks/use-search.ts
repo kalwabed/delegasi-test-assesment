bc5edd3a451808d1a2ae763093fa0f4151e9169f
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-01-08 13:50:10 +0700

    feat: searching for neraca page

--- app/hooks/use-search.ts
+++ app/hooks/use-search.ts
@@ -3,15 +3,34 @@ import { useMemo } from 'react'
 import { useSearchContext } from '~/context'
 import type { Laporan } from '~/types'
 
-export function useSearch(laporan: Laporan) {
+export function useSearch(laporan: Laporan, deep = 1) {
   const { search: searchValue } = useSearchContext()
 
   const searchResult = useMemo(() => {
     if (searchValue) {
-      return (laporan.details ?? laporan.children).filter(detail => {
-        return detail.label.toLowerCase().includes(searchValue.toLowerCase())
-      })
+      if (deep > 1) {
+        const result: Laporan[] = []
+        laporan.details?.forEach(detail => {
+          const searchedDetails = detail.details?.filter(subDetail => {
+            return subDetail.label.toLowerCase().includes(searchValue.toLowerCase())
+          })
+
+          if (searchedDetails?.length) {
+            result.push({
+              ...detail,
+              details: searchedDetails
+            })
+          }
+        })
+
+        return result
+      } else {
+        return laporan.details.filter(detail => {
+          return detail.label.toLowerCase().includes(searchValue.toLowerCase())
+        })
+      }
     }
+
     return laporan.details
   }, [searchValue, laporan])
 
--- app/routes/__dashboard/neraca.tsx
+++ app/routes/__dashboard/neraca.tsx
@@ -4,6 +4,7 @@ import { useLoaderData } from '@remix-run/react'
 import cardIcons from '~/components/card-icons'
 import DataCard from '~/components/data-card'
 import SectionCard from '~/components/section-card'
+import { useSearch } from '~/hooks/use-search'
 import type { Laporan } from '~/types'
 import { formatCurrency } from '~/utils/currency'
 
@@ -14,29 +15,34 @@ export const loader = async (): Promise<Laporan> => {
 
 const NeracaPage = () => {
   const laporan = useLoaderData<typeof loader>()
+  const { laporan: filteredNeraca } = useSearch(laporan, 2)
 
   return (
     <Flex flexDir="column" gap={8}>
       <SectionCard {...laporan} />
 
-      {laporan.details.map((item, i1) => (
-        <div key={item.label}>
-          <Flex flexDir="column" mb={3}>
-            <Heading>{item.label}</Heading>
-            <Text fontSize="xs">{formatCurrency(item.value)}</Text>
-          </Flex>
-          <SimpleGrid templateColumns="repeat(auto-fill, minmax(150px, 1fr))" spacing={5}>
-            {item.details.map((detail, i2) => (
-              <DataCard
-                key={detail.label}
-                bgColor="purple.100"
-                icon={i1 == 1 ? cardIcons[i2 + 2] : cardIcons[i2]}
-                {...detail}
-              />
-            ))}
-          </SimpleGrid>
-        </div>
-      ))}
+      {filteredNeraca?.length > 0 ? (
+        filteredNeraca.map((item, i1) => (
+          <div key={item.label}>
+            <Flex flexDir="column" mb={3}>
+              <Heading>{item.label}</Heading>
+              <Text fontSize="xs">{formatCurrency(item.value)}</Text>
+            </Flex>
+            <SimpleGrid templateColumns="repeat(auto-fill, minmax(150px, 1fr))" spacing={5}>
+              {item.details.map((detail, i2) => (
+                <DataCard
+                  key={detail.label}
+                  bgColor="purple.100"
+                  icon={i1 == 1 ? cardIcons[i2 + 2] : cardIcons[i2]}
+                  {...detail}
+                />
+              ))}
+            </SimpleGrid>
+          </div>
+        ))
+      ) : (
+        <Text>Maaf, data tidak ditemukan</Text>
+      )}
     </Flex>
   )
 }
