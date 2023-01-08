9b98591f514ed679706887a6d5ab860220779356
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-01-08 08:32:42 +0700

    feat: add modal detail

--- app/components/data-card.tsx
+++ app/components/data-card.tsx
@@ -1,4 +1,26 @@
-import { Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stat,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  useDisclosure
+} from '@chakra-ui/react'
+import { HiChevronRight } from 'react-icons/hi2'
+
 import { formatCurrency } from '~/utils/currency'
 import type { Laporan } from '../routes/__dashboard/index'
 
@@ -8,7 +30,8 @@ interface DataCardProps extends Laporan {
 }
 
 const DataCard = (props: DataCardProps) => {
-  const { bgColor, icon, label, value } = props
+  const { bgColor, icon, label, value, details } = props
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <Card bgColor={bgColor} shadow="md">
@@ -23,6 +46,34 @@ const DataCard = (props: DataCardProps) => {
           {formatCurrency(value)}
         </Text>
       </CardBody>
+      <CardFooter justify="end" pt={0}>
+        {details.length > 0 ? (
+          <Button rightIcon={<HiChevronRight />} onClick={onOpen} variant="ghost" color="gray.500" size="xs">
+            Detail
+          </Button>
+        ) : null}
+      </CardFooter>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent mx={2}>
+          <ModalHeader>{label}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {details.map(detail => (
+              <Flex key={detail.label} shadow="md" mb={2}>
+                <Stat p={2}>
+                  <StatLabel>{detail.label}</StatLabel>
+                  <StatNumber fontSize="md">{formatCurrency(detail.value)}</StatNumber>
+                  <StatHelpText>
+                    {new Intl.DateTimeFormat('id', { dateStyle: 'medium' }).format(new Date(detail.month))}
+                  </StatHelpText>
+                </Stat>
+              </Flex>
+            ))}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Card>
   )
 }
